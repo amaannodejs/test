c4be488b7f15faae55e24f010fff6369002a5ed7
amaannodejs <amaan_nodejs@excellencetechnologies.in>
2022-06-24 11:53:01 +0530

    question1:part2 user profile

--- app.js
+++ app.js
@@ -32,10 +32,49 @@ const seedUser=()=>{
         console.log(err)
     })
 }
-for(let i=0; i<5;i++){
-    seedUser()
-}
+// for(let i=0; i<5;i++){
+//     seedUser()
+// }
 
+
+//seeding userprofile
+const seedprofile=async ()=>{
+    const users= await User.find()
+    if(!users){return console.log('no user found')}
+    users.forEach(user=>{
+        Userprofile.findOne({"user_id":user._id}).then(userprofile=>{
+            console.log(userprofile)
+            if(!userprofile){
+                let userp= new Userprofile({
+                    "user_id":user._id,
+                    "dob":faker.date.birthdate(),
+                    "mobile_no":faker.phone.number()
+                })
+                userp.save().then(userp=>{
+                    //console.log(userp)
+                }).catch(err=>{
+                    console.log(err)
+                })
+            }
+        }).catch(err=>{console.log(err)})
+       
+    })
+    
+}
+seedprofile()
+
+//____________Question 1 End______________________
+
+
+
+
+// const date=faker.date.birthdate()
+// date.setMonth(2)
+// date.setFullYear(2005)
+// date.setDate(02)
+// console.log(date)
+
+//routes
 app.get('/',(req,res)=>{
     res.send('server is up!')
 })
